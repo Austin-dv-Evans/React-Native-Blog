31ac02cccc848f883bd7b0eb6415fe61a14a38a5
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-11-30 17:38:29 -0700

    callback functions on edit and add for navigation

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,16 +4,22 @@ import { Context } from '../context/BlogContext'
 import BlogPostForm from '../Components/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
-    const { state } = useContext(Context)
+    const id = navigation.getParam('id')
+    const { state, editBlogPost } = useContext(Context)
 
 
     const blogPost = state.find(
-        (blogPost) => blogPost.id === navigation.getParam('id')
+        (blogPost) => blogPost.id === id
     )
 
  
 
-    return <BlogPostForm />
+    return <BlogPostForm
+        initialValues={{ title: blogPost.title, content: blogPost.content }}
+        onSubmit={(title, content) => {
+            editBlogPost(id, title, content, () => navigation.pop())
+        }}    
+    />
 }
 
 const styles = StyleSheet.create({
