beb65dc7334e9bbeae54fe24f936fcb30004d8a6
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-03 21:57:23 -0700

    context and json server with ngrok

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,12 @@
 import { useCallback } from 'react'
 import createDataContext from './createDataContext'
+import jsonServer from '../api/jsonServer'
+
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'get_blogposts':
+            return action.payload;
         case 'edit_blogpost' : 
             return state.map((blogPost) => {
                 return blogPost.id === action.payload.id
@@ -18,6 +22,15 @@ const blogReducer = (state, action) => {
                 content: action.payload.content }]
         default:
             return state
+    }
+}
+
+const getBlogPosts = dispatch => {
+    return async () => {
+       const response =  await jsonServer.get('/blogposts')
+       
+       dispatch({ type: 'get_blogposts', payload: response.data })
+       
     }
 }
 
@@ -50,6 +63,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    { addBlogPost, deleteBlogPost, editBlogPost }, 
-    [{title: 'Test Post', content: 'Test Content', id: 1}],
+    { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts }, 
+    [],
 )
